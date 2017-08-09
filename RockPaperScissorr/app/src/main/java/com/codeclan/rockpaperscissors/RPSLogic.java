678dbe83bc33faf0c4e492f9fac007f22086b482
Chris Hunter <orkem2@gmail.com>
2017-08-09 10:53:29 +0100

    created test for winnerIs

---
+++ RockPaperScissorr/app/src/main/java/com/codeclan/rockpaperscissors/RPSLogic.java
@@ -0,0 +1,14 @@
+package com.codeclan.rockpaperscissors;
+
+/**
+ * Created by user on 09/08/2017.
+ */
+
+public class RPSLogic {
+
+    public static String winnerIs(String userChoice, String computerChoice){
+        
+
+        return winner;
+    }
+}
---
+++ RockPaperScissorr/app/src/test/java/com/codeclan/rockpaperscissors/RPSLogicTest.java
@@ -0,0 +1,28 @@
+package com.codeclan.rockpaperscissors;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by user on 09/08/2017.
+ */
+
+public class RPSLogicTest {
+
+    String userChoice;
+    String computerChoice;
+
+    @Before
+    public void before(){
+        computerChoice = "Rock";
+    }
+
+    @Test
+    public void canPickWinnerRockVPaper(){
+        userChoice = "Paper";
+        assertEquals("User Wins", RPSLogic.winnerIs(userChoice, computerChoice));
+    }
+
+}
